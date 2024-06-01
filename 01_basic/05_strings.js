7821e2fd35b9a35dc305b751bbc8f15b61eeb9a7
Naved Ali <naved10055@gamil.com>
2024-06-01 15:54:49 +0530

    05 string js complete

--- 01_basic/05_strings.js
+++ 01_basic/05_strings.js
@@ -4,11 +4,17 @@ const repoCount = 50
 // console.log(name + repoCount + " value")
 
 // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
-const gameName = new String("hiteshhc")
+const gameName = new String("hitesh-hc")
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+const newString = gameName.substring(0, 4)
+console.log(newString)
+
+const anotherStirng = gameName.slice(-8, 4)
+console.log(anotherStirng)
