441b2fa93a4a670fdc83e3b0d05e626970cae16c
Naved Ali <naved10055@gamil.com>
2024-06-03 08:00:19 +0530

    json

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -45,10 +45,34 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'))
+// console.log(tinderUser.hasOwnProperty('isLogged'))
+
+
+const course = {
+    coursename: "Js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor)
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in Hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
