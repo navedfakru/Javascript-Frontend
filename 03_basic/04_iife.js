3cd168743d52ecc6756d50ce381a2bfdb0635b1f
Naved Ali <naved10055@gamil.com>
2024-06-04 14:11:41 +0530

    call stack in browser one two three

--- 03_basic/04_iife.js
+++ 03_basic/04_iife.js
@@ -1,11 +1,44 @@
 // Immediately Invoked Function Expressions (IIFE)
 
-(function chai(){
-    // named IIFE
-    console.log(`DB CONNECTED`);
-})();
+// (function chai(){
+//     // named IIFE
+//     console.log(`DB CONNECTED`);
+// })();
 
-((name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-})('hitesh');
+// ((name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// })('hitesh');
 
+
+let val1 = 10;
+let val2 = 5;
+
+function add(num1, num2){
+    let total = num1 + num2
+    return total
+}
+
+// let result = add(val1, val2)
+// console.log(result)
+
+// function fun() {
+//     fun()
+// }
+
+// fun()
+
+function one(){
+    console.log("One")
+    two()
+}
+function two(){
+    console.log("Two")
+    three()
+}
+function three(){
+    console.log("Three")
+}
+
+one()
+two()
+three()
