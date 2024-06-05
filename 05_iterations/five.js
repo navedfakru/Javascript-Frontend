c2ff5d1972f4f9ef3f1d82b81263152f00b7f039
Naved Ali <naved10055@gamil.com>
2024-06-05 10:02:37 +0530

    for each for in for of loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,38 @@
+const codding = ['js', 'ruby', 'java', 'python', 'cpp']
+
+// codding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// codding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item) {
+//     console.log(item);
+// }
+
+// codding.forEach(printMe)
+
+// codding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "Java"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName);
+})
