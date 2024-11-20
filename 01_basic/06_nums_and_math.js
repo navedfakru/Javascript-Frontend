0752ae843d3a6e18d26f250fd3e41b6d1ef9f55e
Naved Ali <naved10055@gmail.com>
2024-11-20 22:31:50 +0530

    mat

--- 01_basic/06_nums_and_math.js
+++ 01_basic/06_nums_and_math.js
@@ -1,4 +1,4 @@
-const score = 400
+/* const score = 400
 const balance = new Number(100)
 // console.log(balance);
 
@@ -30,4 +30,22 @@ console.log(Math.floor(Math.random()*10) + 1);
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random()* (max - min + 1)) + min)
+console.log(Math.floor(Math.random()* (max - min + 1)) + min) */
+
+// const score = 400;
+// console.log(score)
+// const balance = new Number(100);
+// console.log(balance.toFixed(2));
+// const  hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.round(4.6))
+// console.log(Math.random())
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
