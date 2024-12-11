be8c661c1af03d1b46e0a04ad3bd71ff97a8265f
Naved Ali <naved10055@gmail.com>
2024-12-11 12:34:03 +0530

    js javascript

--- Javascript/05_iterations/two.js
+++ Javascript/05_iterations/two.js
@@ -18,3 +18,23 @@
 //     score++
 // } while (score <= 10);
 
+
+let index = 0;
+
+// while (index <= 10) {
+//   console.log(`Value of index is ${index}`);
+//   index = index + 2
+// }
+
+
+let myArray = ['flash', 'batman', 'superman']
+let arr = 0;
+// while(arr < myArray.length){
+//   console.log(`Value of ${myArray[arr]}`)
+//   arr++;
+// }
+let score = 1;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10)
