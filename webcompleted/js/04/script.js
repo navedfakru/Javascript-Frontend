ce6fb75c18b1b97a9f2075a32f1781c4b35fc6d0
Naved <naved10055@gmail.com>
2025-02-27 18:03:14 +0530

    css learning

---
+++ webcompleted/js/04/script.js
@@ -0,0 +1,7 @@
+// window.alert("This is javasript")
+// console.log("Hello World")
+// document.write("Naved")
+
+document.getElementById("h2").innerHTML = '<h1>Hello World</h1>'
+document.getElementById("jstut").innerText = "Js with naved"
+console.log()
---
+++ webcompleted/js/05/script.js
@@ -0,0 +1,16 @@
+let x, y, z;
+x = 5;
+y = 4;
+z = x + y;
+console.log(z)
+document.getElementById("demo").innerHTML = 
+"Hello Dolly.";
+setTimeout(() => {
+  console.log("Naved")
+}, 3000);
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Hello Dolly!";
+  document.getElementById("demo2").innerHTML = "How are you?";
+}
+
+console.log("Helllo")
